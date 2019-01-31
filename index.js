6d1a2a02544ad63c17df0257c55b60d1be1f5a78
kalenkevich <alexey.kalenkevich@mail.ru>
2019-01-31 18:57:14 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 console.log('Hello world');
-console.log('Hello Nikita from Lesha');
-console.log('Papa krytoy');
-console.log('Леша крутой');
-console.log('У МЕНЯ ПОЛУЧИЛОСЬ!!!!!!');
+var x=5*5;
+console.log(x);
