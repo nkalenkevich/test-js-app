c227dbe381355512f604008d0631b1ea694a55e6
kalenkevich <alexey.kalenkevich@mail.ru>
2019-02-01 19:58:52 +0300

    решил 1 задачу

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let name = 'Nikita';
 let x = 'smart';
 let y = 'boy';
 let s = ' ';
-const nikitaResult = null;
+const nikitaResult =`${name} ${x} ${y}`
 
 console.assert('Nikita smart boy' === nikitaResult);
 console.log('ok');
