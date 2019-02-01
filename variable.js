3f5178b7c3090cd8b61d0d6cc2fb5002f178bdc9
Alexey Kalenkevich <alexei.kalenkevich@talenya.com>
2019-02-01 20:13:59 +0300

    task 2: if

--- variable.js
+++ variable.js
@@ -2,7 +2,7 @@ let name = 'Nikita';
 let x = 'smart';
 let y = 'boy';
 let s = ' ';
-const nikitaResult =`${name} ${x} ${y}`
+const nikitaResult =`${name} ${x} ${y}`;
 
 console.assert('Nikita smart boy' === nikitaResult);
 console.log('ok');
